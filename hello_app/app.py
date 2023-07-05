060c65bdd6a13a518c92cde8255959e4eda2689d
Tqgeng <134533682+Tqgeng@users.noreply.github.com>
2023-07-05 17:26:13 +0300

    Update app.py

--- hello_app/app.py
+++ hello_app/app.py
@@ -1,4 +1,5 @@
 import os
+from datatime import date
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -9,3 +10,7 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+today=date.today()
+formatted_date=today.strftime('%Y-%m-%d')
+print(formatted_date)
